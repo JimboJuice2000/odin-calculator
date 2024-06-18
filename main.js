05f9fc9d42ef8ce8d878b3c3f5f269c02c676dde
JimboJuice2000 <gauravgrox@gmail.com>
2024-06-18 18:56:29 +0530

    Finsihed it

---
+++ main.js
@@ -0,0 +1,39 @@
+const display = document.querySelector("#display");
+
+// function appendToDisplay(input) {
+//     display.value +=input;
+// }
+
+function appendToDisplay(input) {
+    const operators = ['+', '-', '*', '/'];
+    const lastChar = display.value.slice(-1);
+
+    // If the input is operator and the last character is operator, u replace it
+    if (operators.includes(input) && operators.includes(lastChar)) {
+        display.value = display.value.slice(0, -1) + input;
+    } else {
+        display.value += input;
+    }
+
+    // If the last character was an operator and u just added a number, calculate it
+    if (operators.includes(lastChar) && !operators.includes(input)) {
+        calculate();
+    }
+}
+
+function clearDisplay() {
+    display.value="";
+}
+
+function calculate() {
+    
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "ERROR"
+    }
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
