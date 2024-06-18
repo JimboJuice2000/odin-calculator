e810603a5f02feea24341a86feab0814ad09f2f5
JimboJuice2000 <gauravgrox@gmail.com>
2024-06-18 20:44:38 +0530

    bug fixing

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const display = document.querySelector("#display");
 
-// function appendToDisplay(input) {
-//     display.value +=input;
-// }
-
 function appendToDisplay(input) {
-    const operators = ['+', '-', '*', '/'];
-    const lastChar = display.value.slice(-1);
-
-    // If the input is operator and the last character is operator, u replace it
-    if (operators.includes(input) && operators.includes(lastChar)) {
-        display.value = display.value.slice(0, -1) + input;
-    } else {
-        display.value += input;
-    }
-
-    // If the last character was an operator and u just added a number, calculate it
-    if (operators.includes(lastChar) && !operators.includes(input)) {
-        calculate();
-    }
+    display.value +=input;
 }
+
+// function appendToDisplay(input) {
+//     const operators = ['+', '-', '*', '/'];
+//     const lastChar = display.value.slice(-1);
+
+//     // If the input is operator and the last character is operator, u replace it
+//     if (operators.includes(input) && operators.includes(lastChar)) {
+//         display.value = display.value.slice(0, -1) + input;
+//     } else {
+//         display.value += input;
+//     }
+
+//     // If the last character was an operator and u just added a number, calculate it
+//     if (operators.includes(lastChar) && !operators.includes(input)) {
+//         calculate();
+//     }
+// }
 
 function clearDisplay() {
     display.value="";
